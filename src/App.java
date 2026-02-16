2db2ea6e01784c8975f2fe20a29783a8caf7335f
mxikai <kmrdmai@gmail.com>
2026-02-16 13:33:13 +0800

    flatlaf

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
 import gui.Student_Panel;
 import javax.swing.*;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
 public class App {
     public static void main(String[] args) {
+
+        try {
+            FlatLightLaf.setup(); 
+        } catch (Exception e) {
+            System.out.println("Look and Feel failed to initialize");
+        }
+        
         JFrame frame = new JFrame("Simple Student Information System");
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
